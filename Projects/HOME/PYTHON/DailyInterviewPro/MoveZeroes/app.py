12fbe8fe9da0c8eec7e66eba6be563d778780447
Yash Djson Dookun <dookun88@gmail.com>
2020-04-09 12:05:55 +0400

    Delete app.py

--- Projects/HOME/PYTHON/DailyInterviewPro/MoveZeroes/app.py
+++
@@ -1,9 +0,0 @@
-class Solution():
-    def moveZeros(self, nums):
-        for i in range(len(nums)):
-            for j in range(0,(len(nums) - 1)):
-                if (nums[j] == 0):
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-nums = [0,0,0,2,0,1,3,4,0,0]
-Solution().moveZeros(nums)
-print(nums)
